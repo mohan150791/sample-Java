e9c052bc67030b5a42e843c0296b46064c705809
unknown <flykarma@gmail.com>
2022-08-11 08:59:05 +0530

    sample java

---
+++ src/org/sample/Samplejavaapp.java
@@ -0,0 +1,11 @@
+package org.sample;
+
+public class Samplejavaapp {
+	public static void main(String[] args) {
+		System.out.println(1);
+		System.out.println(2);
+		System.out.println(3);
+		System.out.println(4);
+	}
+
+}
